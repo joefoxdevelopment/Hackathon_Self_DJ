a61ce7b788f515e9fa9159b036a9cad7c6238d64
= <joefoxdevelopment@gmail.com>
2018-01-25 10:40:21 +0000

    Add equalisation management

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var pickAndPlay = function(source) {
-    var audio     = window.players[source - 1];
+    var audio     = window.selfdj.players[source];
     var container = document.querySelector(".source" + source);
     var sound     = document.querySelector(".picker"+source+"[type=file]").files[0];
     var reader    = new FileReader();
@@ -11,27 +11,94 @@ var pickAndPlay = function(source) {
 };
 
 var calcVol = function() {
-    var player2 = document.getElementById("player2");
     var slider  = document.getElementById("mixer");
+    var vol0    = document.getElementById("vol0");
     var vol1    = document.getElementById("vol1");
-    var vol2    = document.getElementById("vol2");
 
-    window.selfdj.players[1].volume = ((vol2.value/vol2.max) * slider.value)/slider.max;
-    window.selfdj.players[0].volume = ((vol1.value/vol1.max) * (slider.max - slider.value))/slider.max;
+    window.selfdj.players[1].volume = ((vol1.value/vol1.max) * slider.value)/slider.max;
+    window.selfdj.players[0].volume = ((vol0.value/vol0.max) * (slider.max - slider.value))/slider.max;
 }
+
+var changeEq = function(slider, type, source) {
+    var value = slider.value / slider.max;
+
+    var gains = window.selfdj.gains[source];
+
+    switch(type)
+    {
+        case 'lowGain': gains.lGain.gain.value = value; break;
+        case 'midGain': gains.mGain.gain.value = value; break;
+        case 'highGain': gains.hGain.gain.value = value; break;
+    }
+};
 
 window.addEventListener('load', function() {
     window.selfdj = {};
 
     window.selfdj.players = [
+        document.getElementById("player0"),
         document.getElementById("player1"),
-        document.getElementById("player2"),
     ];
 
-    window.selfdj.context  = new webAudioContext();
+    window.selfdj.context  = new webkitAudioContext();
 
     window.selfdj.sources = [
         window.selfdj.context.createMediaElementSource(window.selfdj.players[0]),
         window.selfdj.context.createMediaElementSource(window.selfdj.players[1]),
     ];
+
+    window.selfdj.gaindb = -40.0;
+    window.selfdj.gainBandSplit = [360, 3600];
+
+    window.selfdj.gains = [];
+
+    window.selfdj.sources.forEach(function(source, index) {
+        window.selfdj.gains[index] = [];
+
+        var hBand = window.selfdj.context.createBiquadFilter();
+        hBand.type = "lowshelf";
+        hBand.frequency.value = window.selfdj.gainBandSplit[0];
+        hBand.gain.value = window.selfdj.gaindb;
+
+        var hInvert = window.selfdj.context.createGain();
+        hInvert.gain.value = -1.0;
+
+        var lBand = window.selfdj.context.createBiquadFilter();
+        lBand.type = "lowshelf";
+        lBand.frequency.value = window.selfdj.gainBandSplit[1];
+        lBand.gain.value = window.selfdj.gaindb;
+
+        var lInvert = window.selfdj.context.createGain();
+        lInvert.gain.value = -1.0;
+
+        var mBand = window.selfdj.context.createGain();
+
+        hInvert.connect(mBand);
+        lInvert.connect(mBand);
+
+        hBand.connect(hInvert);
+        lBand.connect(lInvert);
+
+        source.connect(hBand);
+        source.connect(mBand);
+        source.connect(lBand);
+
+        var lGain = window.selfdj.context.createGain();
+        var mGain = window.selfdj.context.createGain();
+        var hGain = window.selfdj.context.createGain();
+
+        hBand.connect(hGain);
+        mBand.connect(mGain);
+        lBand.connect(lGain);
+
+        var sum = window.selfdj.context.createGain();
+        lGain.connect(sum);
+        mGain.connect(sum);
+        hGain.connect(sum);
+        sum.connect(window.selfdj.context.destination);
+
+        window.selfdj.gains[index]['hGain'] = hGain;
+        window.selfdj.gains[index]['mGain'] = mGain;
+        window.selfdj.gains[index]['lGain'] = lGain;
+    });
 });
